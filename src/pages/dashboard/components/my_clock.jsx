87c22bf20aa9b38b45f5cb8fe948ba8eed1e4b37
Tbless01 <olaitanblessinge@gmail.com>
2023-05-23 12:42:31 +0100

    new

--- src/pages/dashboard/components/my_clock.jsx
+++ src/pages/dashboard/components/my_clock.jsx
@@ -3,10 +3,10 @@ import "../../dashboard/styles/my_clock.css";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
-  const [day, setDay] = useState("PM");
-  // setDay("AM")
+  const [time2] = useState(new Date());
+  const [day, setDay] = useState("AM");
 
-useEffect(() => {
+  useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date());
     }, 1000);
@@ -16,10 +16,11 @@ useEffect(() => {
   let hours = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
-useEffect(()=>{
-  hours <= 12 && day === "PM"? setDay("AM"): setDay("PM") ;
-}, [])
-  
+  let hours2 = time2.getHours();
+
+  useEffect(() => {
+    hours2 >= 12 ? setDay("PM") : setDay("AM");
+  }, [hours2]);
 
   if (hours > 12) {
     hours = hours - 12;
@@ -31,14 +32,17 @@ useEffect(()=>{
 
   return (
     <div className="container">
-    <div className="mainContain">
-      <h1 className="clock">
-        {hours < 10 ? `0${hours}` : hours}:
-        {minutes < 10 ? `0${minutes}` : minutes}
-        <span className="seconds">{seconds < 10 ? `0${seconds}` : seconds}</span> <span className="day">{day}</span>
-      </h1>
+      <div className="mainContain">
+        <h1 className="clock">
+          {hours < 10 ? `0${hours}` : hours}:
+          {minutes < 10 ? `0${minutes}` : minutes}
+          <span className="seconds">
+            {seconds < 10 ? `0${seconds}` : seconds}
+          </span>{" "}
+          <span className="day">{day}</span>
+        </h1>
+      </div>
     </div>
-     </div>
   );
 }
 
